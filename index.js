d0644fe074662ceddca854f9ccb1a2d69c8d9459
Daisy Yi <daisyyi@rocketmail.com>
2022-08-10 15:07:53 -0700

    commit tree 28026607c98aaf3a1aa41812b3b5ff22013ec1b4

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 =31;
+let num2 =2;
+let random = 1;
+let num3 = 16;
+let num4 = 6;
+let mod = 4;
+let max = 20;
 
-// Write your code here
+const multiply = num1 * num2;
+
+random = Math.ceiling(Math.random());
+
+mod = num3 %= num4
+
+max = Math.max(1, 2, 20);
